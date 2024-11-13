b75cdb5feebfad8f69f840ab05d441ff46016af2
aaron <aaron@moduparking.com>
2024-11-13 18:27:03 +0900

    feat: selectbox 설정

--- src/components/shared/Accordion.tsx
+++ src/components/shared/Accordion.tsx
@@ -25,7 +25,7 @@ const AccordionItem = ({ title, children }: AccordionItemProps) => {
       >
         <span>{title}</span>
         <span className="text-slate-800 transition-transform duration-300">
-          {isOpen ? (
+          {!isOpen ? (
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 16 16"
---
+++ src/components/shared/SelectBox.tsx
@@ -0,0 +1,68 @@
+import { useForm, Controller } from "react-hook-form";
+
+const SelectBox = ({ name, label, options, control }) => {
+  return (
+    <div className="max-w-sm">
+      {label && (
+        <label
+          htmlFor={name}
+          className="block text-sm font-medium text-gray-700 mb-1"
+        >
+          {label}
+        </label>
+      )}
+      <Controller
+        name={name}
+        control={control}
+        defaultValue=""
+        render={({ field }) => (
+          <select
+            {...field}
+            id={name}
+            className="border border-neutral-100 text-sm min-w-8 rounded-sm block py-2 pl-2"
+          >
+            {options.map((option) => (
+              <option key={option.value} value={option.value}>
+                {option.label}
+              </option>
+            ))}
+          </select>
+        )}
+      />
+    </div>
+  );
+};
+
+export default SelectBox;
+
+// const Form = () => {
+//   const { handleSubmit, control } = useForm();
+
+//   const onSubmit = (data) => {
+//     console.log(data); // 제출된 데이터 확인
+//   };
+
+//   return (
+//     <form onSubmit={handleSubmit(onSubmit)}>
+//       <SelectComponent
+//         name="country"
+//         label="Select an option"
+//         control={control}
+//         options={[
+//           { value: "US", label: "United States" },
+//           { value: "CA", label: "Canada" },
+//           { value: "FR", label: "France" },
+//           { value: "DE", label: "Germany" },
+//         ]}
+//       />
+//       <button
+//         type="submit"
+//         className="mt-4 bg-blue-500 text-white rounded-lg px-4 py-2"
+//       >
+//         Submit
+//       </button>
+//     </form>
+//   );
+// };
+
+// export default Form;

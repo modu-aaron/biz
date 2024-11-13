1178c4daf0d4d032c82d4391cdd25cd002a0a97e
aaron <aaron@moduparking.com>
2024-11-13 14:12:18 +0900

    refactor: img 속성 camelCase 적용

--- src/pages/ticket/Ticket.tsx
+++ src/pages/ticket/Ticket.tsx
@@ -131,9 +131,9 @@ const Ticket = () => {
                     data-slot="icon"
                   >
                     <path
-                      fill-rule="evenodd"
+                      fillRule="evenodd"
                       d="M11.78 5.22a.75.75 0 0 1 0 1.06L8.06 10l3.72 3.72a.75.75 0 1 1-1.06 1.06l-4.25-4.25a.75.75 0 0 1 0-1.06l4.25-4.25a.75.75 0 0 1 1.06 0Z"
-                      clip-rule="evenodd"
+                      clipRule="evenodd"
                     ></path>
                   </svg>
                 </a>
@@ -191,9 +191,9 @@ const Ticket = () => {
                     data-slot="icon"
                   >
                     <path
-                      fill-rule="evenodd"
+                      fillRule="evenodd"
                       d="M8.22 5.22a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.75.75 0 0 1-1.06-1.06L11.94 10 8.22 6.28a.75.75 0 0 1 0-1.06Z"
-                      clip-rule="evenodd"
+                      clipRule="evenodd"
                     ></path>
                   </svg>
                 </a>

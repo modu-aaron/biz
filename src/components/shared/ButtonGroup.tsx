74904192ecbc93edf9e15ee899d8633dbf92aa62
aaron <aaron@moduparking.com>
2024-11-27 15:50:09 +0900

    feat: Button Group 컴포넌트 제작

---
+++ src/components/shared/ButtonGroup.tsx
@@ -0,0 +1,29 @@
+import Button from "./Button";
+
+const ButtonGroup = ({ buttons }) => {
+  return (
+    <div className="inline-flex rounded-sm shadow-sm" role="group">
+      {buttons.map((button, index) => {
+        const isFirst = index === 0;
+        const isLast = index === buttons.length - 1;
+        const additionalClasses = isFirst
+          ? "rounded-s-lg"
+          : isLast
+          ? "rounded-e-lg"
+          : "border-t border-b";
+
+        return (
+          <Button
+            key={index}
+            text={button.text}
+            onClick={button.onClick}
+            isRounded={false}
+            additionalClasses={additionalClasses}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default ButtonGroup;

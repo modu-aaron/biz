cafbe74b64f8f04fc5e171822e9de8344c1d980a
aaron <aaron@moduparking.com>
2024-12-10 17:42:56 +0900

    feat: partner dto 정의

---
+++ src/services/api/partner/partner.dto.ts
@@ -0,0 +1,43 @@
+import { Type } from "class-transformer";
+import { Paginated } from "../dto/common.dto";
+
+export class UserDto {
+  puSeq!: number;
+  name!: string;
+  phone!: string;
+  roleName!: string;
+  status!: string;
+  activatedAt!: Date;
+}
+
+export class PartnerCardDto {
+  name!: string;
+  number!: string | null;
+}
+
+export class PartnerDto {
+  pSeq!: number;
+  name!: string;
+  address!: string;
+  paymentType!: number;
+  @Type(() => PartnerCardDto)
+  partnerCard!: PartnerCardDto | null;
+  createdAt!: Date;
+}
+
+export class PartnerMemberDto extends Paginated {
+  @Type(() => UserDto)
+  results!: UserDto[];
+}
+
+export class AddCardPayloadDto {
+  cardNumber!: string;
+  expYear!: string;
+  expMonth!: string;
+  authNumber!: string;
+}
+
+export class UpdateUserPayloadDto {
+  name!: string;
+  phone!: string;
+}

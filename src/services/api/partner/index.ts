57ad15fb5a88d1ef05b113f4d8b0679b20256642
aaron <aaron@moduparking.com>
2024-12-10 17:42:44 +0900

    feat: partner route api 추가

---
+++ src/services/api/partner/index.ts
@@ -0,0 +1,56 @@
+import type { UpdateUserPayloadDto, AddCardPayloadDto } from "./partner.dto";
+import { UserDto, PartnerDto, PartnerMemberDto } from "./partner.dto";
+import axios from "../../axios/index";
+import { plainToInstance } from "class-transformer";
+
+/**
+ * 소속 파트너 정보 조회
+ * @return { PartnerDto }
+ }
+ */
+const getPartner = async (): Promise<PartnerDto> => {
+  const { data } = await axios.get<PartnerDto>("/");
+  return plainToInstance(PartnerDto, data);
+};
+
+/**
+ * 멤버조회
+ * @return {results: UserDto[]}
+ */
+const getMembers = async (): Promise<PartnerMemberDto> => {
+  const { data } = await axios.get<PartnerMemberDto>("/user");
+  return plainToInstance(PartnerMemberDto, data);
+};
+
+/**
+ * 프로필조회
+ * @return { UserDto }
+ */
+const getUser = async (): Promise<UserDto> => {
+  const { data } = await axios.get<UserDto>("/user/profile");
+  return plainToInstance(UserDto, data);
+};
+
+/**
+ * 프로필수정
+ * @payload { UpdateUserPayloadDto }
+ */
+const updateUser = async (payload: UpdateUserPayloadDto) => {
+  await axios.put("/user/profile", payload);
+};
+
+/**
+ * 카드등록
+ * @payload { AddCardPayloadDto }
+ */
+const addCard = async (payload: AddCardPayloadDto) => {
+  await axios.post("/card", payload);
+};
+
+export default {
+  getUser,
+  getPartner,
+  getMembers,
+  addCard,
+  updateUser,
+};

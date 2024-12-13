bfebc63e7ab050d49d4e3a4afd80c7014df13cb0
aaron <aaron@moduparking.com>
2024-12-13 10:04:18 +0900

    feat: 신용카드 UI

---
+++ src/components/partner/Card.tsx
@@ -0,0 +1,30 @@
+import { FaCcVisa } from "react-icons/fa";
+
+const Card = ({ cardNumber }: { cardNumber: string }) => {
+  return (
+    <>
+      <div className="relative border w-32 md:w-full aspect-square rounded-md overflow-hidden">
+        <img
+          src="https://ca.slack-edge.com/T04GJSZC2-U06DV3V3TL6-f19097d81964-512"
+          alt="프로필"
+          className="object-cover h-full md:object-fill"
+        />
+      </div>
+
+      <div className="flex flex-col gap-0 mt-4">
+        <h1 className="text-xl font-semibold tracking-tight leading-tight">
+          {cardNumber || "**** **** **** ****"}
+        </h1>
+        <p className="text-sm font-mono">MM/YY</p>
+      </div>
+      <div className="mt-auto flex justify-between items-center">
+        <span className="text-[0.6rem] font-medium px-2 py-[3px] rounded-sm">
+          SOCAR
+        </span>
+        <FaCcVisa size={28} className="opacity-70" />
+      </div>
+    </>
+  );
+};
+
+export default Card;

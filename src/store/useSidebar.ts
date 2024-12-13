5e613a59c10a9cc4edc33190f4d1bc15e2236684
aaron <aaron@moduparking.com>
2024-12-13 15:04:53 +0900

    feat: 사이드바 상태 체크 state 설정

--- src/store/useSidebar.ts
+++ src/store/useSidebar.ts
@@ -3,13 +3,17 @@ import { create } from "zustand";
 interface SidebarState {
   isSidebarOpen: boolean;
   isDisplay: boolean;
+  isInitialized: boolean;
   setIsSidebarOpen: (value: boolean) => void;
   setIsDisplay: (value: boolean) => void;
+  initializeSidebar: () => void;
 }
 
 export const useSidebar = create<SidebarState>((set) => ({
   isSidebarOpen: true,
   isDisplay: true,
+  isInitialized: false,
   setIsSidebarOpen: (value: boolean) => set({ isSidebarOpen: value }),
   setIsDisplay: (value: boolean) => set({ isDisplay: value }),
+  initializeSidebar: () => set({ isInitialized: true }),
 }));

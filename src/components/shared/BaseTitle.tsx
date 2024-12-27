a6f088784729478b22ce6a10a0894566ee058a17
aaron <aaron@moduparking.com>
2024-12-27 17:15:45 +0900

    feat: BaseTitle type 정의

--- src/components/shared/BaseTitle.tsx
+++ src/components/shared/BaseTitle.tsx
@@ -1,4 +1,8 @@
-const BaseTitle = ({ text }: { text: string }) => {
+interface BaseTitleProps {
+  text: string;
+}
+
+const BaseTitle = ({ text }: BaseTitleProps) => {
   return (
     <h1 className="py-3 text-left w-full font-semibold text-base sm:text-lg md:text-xl">
       {text}

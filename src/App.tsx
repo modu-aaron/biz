30c8626435ef7edfa87e1f5e2823746c90d4b44b
aaron <aaron@moduparking.com>
2025-01-02 17:55:55 +0900

    refactor: isSignedIn 여부에 따른 redirect 로직

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import MainWrapper from "./components/shared/layout/MainWrapper";
 import { ToastContainer } from "react-toastify";
+import { useAuth } from "@/store/useAuth";
+import { useEffect } from "react";
 
 function App() {
+  const { isSignedIn } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!isSignedIn) {
+      navigate("/signIn");
+    }
+  }, [isSignedIn]);
+
   return (
     <>
       <MainWrapper>

2ddf7e3152edf24e897f2c5d500494a15ad4f110
aaron <aaron@moduparking.com>
2025-01-16 16:36:21 +0900

    feat: react-query setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
 import "reflect-metadata";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./index.css";
-import Router from "./router/Router";
+import Router from "@/router/Router";
 import "react-datepicker/dist/react-datepicker.css";
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Router />
+    <QueryClientProvider client={queryClient}>
+      <Router />
+    </QueryClientProvider>
   </StrictMode>
 );

1092c8f394a40094254032286b6afd0f0db2e158
aaron <aaron@moduparking.com>
2025-02-04 09:39:38 +0900

    refactor: App 컴포넌트에서 인증 관련 코드 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,8 @@
 import { Outlet, useNavigate } from "react-router-dom";
 import MainWrapper from "@/shared/components/layout/MainWrapper";
 import { useAuth } from "@/store/useAuth";
-import { useEffect } from "react";
 
 function App() {
-  const { isSignedIn } = useAuth();
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (!isSignedIn) {
-      navigate("/signIn");
-    }
-  }, [isSignedIn]);
-
   return (
     <>
       <MainWrapper>

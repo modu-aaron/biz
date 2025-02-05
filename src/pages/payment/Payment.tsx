5921f650923a14909089248b84fb0792415b5284
aaron <aaron@moduparking.com>
2025-02-05 16:56:40 +0900

    feat: 결제 상세 페이지에 엑셀 다운로드 기능 추가 및 현재 시간 표시

--- src/pages/payment/Payment.tsx
+++ src/pages/payment/Payment.tsx
@@ -3,9 +3,17 @@ import { useParams } from "react-router-dom";
 import ListTable from "@/shared/components/ListTable";
 import usePaymentDetail from "@/hooks/usePayment";
 import Table from "@/utils/generateTable";
+import { useExcelDownload } from "@/hooks/useExcelDownload";
+import paymentService from "@/services/api/payment";
+import { RequestExcelStatusPayload } from "@/services/api/payment/payment.dto";
+import excelService from "@/services/api/excel";
+import ExcelButton from "@/shared/components/ExcelButton";
+import { convertToDate } from "@/utils/date";
+import { DateFormat } from "@/enums";
 
 const Payments = () => {
   const { id } = useParams();
+  const { excelDownload } = useExcelDownload();
   const data = usePaymentDetail(id || "");
 
   const paymentDetailHeaders = [
@@ -37,12 +45,37 @@ const Payments = () => {
     Table.payment.generatePaymentDetailTableBody(data);
   const orderDetailTableBody = Table.payment.generateOrderItemTableBody(data);
 
+  const onDownloadExcel = async (
+    payload: RequestExcelStatusPayload["reason"]
+  ) => {
+    await excelDownload(
+      () => paymentService.getPaymentOrderItemsExcel(Number(id), payload),
+      excelService.getExcelStatus
+    );
+  };
+
+  const date = convertToDate(new Date(), DateFormat.DAY_YYYY_MM_DD_DASH);
+  const time = convertToDate(new Date(), DateFormat.TIME_HH_MM_SS_COLON);
+  const now = `${date} ${time}`;
+
   return (
     <>
       <BaseTitle text="결제상세" />
       <div className="flex flex-col gap-6">
         <div className="px-6 py-3 border border-gray-100">
-          <h2 className="text-lg font-semibold mb-2">결제 정보</h2>
+          <div className="flex py-2 justify-between items-center">
+            <h2 className="text-lg font-semibold">결제 정보</h2>
+            <div className="flex gap-2 items-center">
+              <p className="text-sm">{now} 기준</p>
+              <ExcelButton
+                text="결제상세 엑셀 다운로드"
+                onClick={onDownloadExcel}
+                payload="주차권"
+                additionalClasses="btn-primary"
+              />
+            </div>
+          </div>
+
           <ListTable
             headers={paymentDetailHeaders}
             body={paymentDetailTableBody}

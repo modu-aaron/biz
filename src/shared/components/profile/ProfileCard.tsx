7f9790ca244c4ddc0b3d96d543658144ed91807a
aaron <aaron@moduparking.com>
2025-02-05 16:51:57 +0900

    feat: 프로필 카드 레이아웃 개선 및 휴대폰 번호 가리기 기능 추가

--- src/shared/components/profile/ProfileCard.tsx
+++ src/shared/components/profile/ProfileCard.tsx
@@ -15,32 +15,34 @@ const ProfileCard = ({ name, phone, role, activatedAt }: ProfileCardProps) => {
   if (activatedAt) {
     day = convertToDate(activatedAt, DateFormat.DAY_YYYY_MM_DD_DASH);
   }
+  //NOTE: 휴대폰 번호 가리기
   if (phone) {
     phone = phone.replace(/(\d{3})(\d{4})(\d{4})/, "$1****$3");
   }
 
   return (
     <>
-      <div className="flex flex-col items-center gap-2">
-        <div className="relative border w-18 md:w-48 aspect-square rounded-full overflow-hidden">
+      <div className="flex w-full items-center gap-12 sm:flex-col sm:gap-2">
+        <div className="relative border w-32 md:w-48 aspect-square rounded-full overflow-hidden">
           <img
             src="https://ca.slack-edge.com/T04GJSZC2-U06DV3V3TL6-f19097d81964-512"
             alt="프로필"
-            className="object-cover h-full md:object-fill"
           />
         </div>
-        <h1 className="text-xl font-semibold tracking-tight leading-tight py-2">
-          {name || "홍길동님"}
-        </h1>
-        <p className="text-sm font-mono">
-          {phone || "휴대폰 번호를 입력해주세요."}
-        </p>
-        <p className="text-xs font-mono">가입일자: {day}</p>
+        <div className="flex gap-2 flex-col">
+          <div className="flex items-center justify-between">
+            <h1 className="text-xl font-semibold tracking-tight leading-tight py-2">
+              {name || "홍길동"}
+            </h1>
+            <span className="text-[0.8rem] font-medium px-2 py-[3px] rounded-sm">
+              {role || "-"}
+            </span>
+          </div>
+          <p className="text-sm font-mono">{phone || "-"}</p>
+          <p className="text-xs font-mono">{day}</p>
+        </div>
       </div>
       <div className="mt-auto flex justify-between items-center pt-4">
-        <span className="text-[0.8rem] font-medium px-2 py-[3px] rounded-sm">
-          {role || "역할을 선택해주세요."}
-        </span>
         <img src={bizSmallLogo} className="object-cover w-10 h-10" />
       </div>
     </>

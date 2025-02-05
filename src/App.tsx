2fc788a900647b3f2df812e0e396aa50592dded2
aaron <aaron@moduparking.com>
2025-02-05 15:29:56 +0900

    feat: 로그인 상태에 따라 사용자를 /signIn으로 리디렉션하고 에러 메시지 표시

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import MainWrapper from "@/shared/components/layout/MainWrapper";
+import { useAuth } from "@/store/useAuth";
+import { useEffect } from "react";
+import { toast } from "react-toastify";
 
 function App() {
+  const { isSignedIn } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!isSignedIn) {
+      navigate("/signIn");
+      toast.error("로그인이 필요합니다.");
+    }
+  }, [isSignedIn]);
+
   return (
     <>
       <MainWrapper>
